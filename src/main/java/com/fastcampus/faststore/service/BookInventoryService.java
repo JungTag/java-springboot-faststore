5cadda6441f32d6e53bc617097e89978d52953ef
JungTag <dbsdudxor121@naver.com>
2021-10-11 16:59:19 +0900

    feat(BookInventoryService): sellBook 메소드 구현

--- src/main/java/com/fastcampus/faststore/service/BookInventoryService.java
+++ src/main/java/com/fastcampus/faststore/service/BookInventoryService.java
@@ -27,6 +27,11 @@ public class BookInventoryService {
     // TODO: 책을 판매하는 메서드를 bookService, bookSaleService 등을 이용하여 구현하세요.
     @Transactional
     public void sellBook(String title) {
+        Book book = bookService.getOrThrow(title);
+        BookSale bookSale = bookSaleService.getOrThrow(book);
+        BookInventory inventory = getOrNew(book);
+        inventory.minusBook(bookSale);
+        bookInventoryRepository.save(inventory);
     }
 
     @Transactional
